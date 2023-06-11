135021ec9f4274107402ee141b5221c3ee6f241b
Sonnydread <alvaradoguille590@gmail.com>
2023-06-11 08:27:40 -0700

    lsDV

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    DV = [0] * list_length
-    for W in range(list_length):
-        try:
-            DV[W] = my_list_1[W] / my_list_2[W]
-        except TypeError:
-            print("wrong type")
-            DV[W] = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            DV[W] = 0
-        except IndexError:
-            print("out of range")
-            DV[W] = 0
+    DV = []
+    try:
+        for W in range(list_length):
+            try:
+                result = my_list_1[W] / my_list_2[W]
+            except TypeError:
+                print("wrong type")
+                result = 0
+            except ZeroDivisionError:
+                print("division by 0")
+                result = 0
+            except IndexError:
+                print("out of range")
+                result = 0
+            DV.append(result)
+    finally:
+        while len(DV) < list_length:
+            DV.append(0)
     return DV
