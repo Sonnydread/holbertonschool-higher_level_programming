b5cbb02b9085ac680e6bd5c729d2791f84468fe8
Sonnydread <alvaradoguille590@gmail.com>
2023-06-17 05:33:41 -0700

    4sq

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """attribute"""
+    def __init__(self, size=0):
+        """size class of da square
+        Args:
+            size (int): must be integer
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
