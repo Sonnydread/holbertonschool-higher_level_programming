bfa67c3a9e2447aeb521a11dec6d66c3c7e73d4b
Sonnydread <alvaradoguille590@gmail.com>
2023-06-25 09:09:46 -0700

    11stu

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by name, lname n age"""
+
+
+class Student:
+    """Clasify by name, last name n age"""
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        try:
+            new = dict(filter(lambda e: e[0] in attrs, self.__dict__.items()))
+            return new
+        except Exception:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+
+        replace = self.__dict__
+        return replace.update(json)
