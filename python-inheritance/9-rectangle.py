8b34edf68c305ed891564e079a8d732c9d7a627a
Sonnydread <alvaradoguille590@gmail.com>
2023-06-24 22:43:47 -0700

    9rek

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""empty class BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """empty BaseGeometry class"""
+
+    def __init__(self, width, height):
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        REK = '[Rectangle] {}/{}'
+        return (REK.format(self.__width, self.__height))
