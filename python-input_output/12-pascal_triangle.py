57398f4a1187f694d1096d78b5e190f7655b7e05
Sonnydread <alvaradoguille590@gmail.com>
2023-06-25 09:28:25 -0700

    pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,12 +5,12 @@
 def pascal_triangle(n):
 
     triangle = []
-    for i in range(n):
-        DUB = []
-        for Z in range(i + 1):
-            if Z == 0 or Z == i:
-                DUB.append(1)
+    for X in range(n):
+        row = []
+        for Z in range(X + 1):
+            if Z == 0 or Z == X:
+                row.append(1)
             else:
-                DUB.append(triangle[i - 1][Z - 1] + triangle[i - 1][Z])
-        triangle.append(DUB)
+                row.append(triangle[X - 1][Z - 1] + triangle[X - 1][Z])
+        triangle.append(row)
     return triangle
