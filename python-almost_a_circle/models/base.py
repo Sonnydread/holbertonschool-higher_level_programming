cac57ef209600f5fba963e14c1a03d8ae20b5759
Sonnydread <alvaradoguille590@gmail.com>
2023-06-29 20:39:10 -0700

    json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Base of all other classes in this project"""
 
 
+import json
+
+
 class Base:
     """Base of all other classes in this project"""
 
@@ -14,3 +17,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        new = []
+        if list_objs is not None:
+            for ele in list_objs:
+                new.append(ele.to_dictionary())
+        with open("{}.json".format(cls.__name__), 'w') as fle:
+            fle.write(cls.to_json_string(new))
