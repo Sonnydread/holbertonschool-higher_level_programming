67183296e570856f4e901c033ae8dc1180675b0c
Sonnydread <alvaradoguille590@gmail.com>
2023-07-27 14:23:58 -0700

    2crear

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create the database hbtn_0d_2 and user_0d_2
+-- the user_0d_2 password should be set to user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
