2e6a5fc09be1e738aea66a866c7b30bb8fdd93e0
Sonnydread <alvaradoguille590@gmail.com>
2023-07-29 05:31:06 -0700

    15com

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- list all comedy shows in the database hbtn_0d_tvshows
+-- results must be sorted in ascending order by the show title
+SELECT tv_shows.title AS title FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
