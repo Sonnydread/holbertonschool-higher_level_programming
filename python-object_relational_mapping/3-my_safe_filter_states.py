9a1eff1d568b743c8c64a1b833d4dbfdee9c2de5
Sonnydread <alvaradoguille590@gmail.com>
2023-08-02 06:41:11 -0700

    3filtr

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     kursor = db.cursor()
-    kursor.execute (sys.argv[4])
+    kursor.execute("""SELECT id, name
+                FROM states
+                WHERE BINARY name = %s
+                ORDER BY id""", (sys.argv[4],))
     rows = kursor.fetchall()
     for row in rows:
         print(row)
