5cff8383b1e3fa4477ff195d1f6bf1a5787cd9e2
Sonnydread <alvaradoguille590@gmail.com>
2023-08-02 05:53:59 -0700

    1filt

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list all states names starting with N"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    konex = MySQLdb.connect(host="localhost", user=argv[1],
+                                 passwd=argv[2], db=argv[3], port=3306)
+    kurs = konex.kurs()
+    request = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id"
+    rows_number = kurs.execute(request)
+
+    for row in range(rows_number):
+        print(kurs.fetchone())
+
+    kurs.close()
+    konex.close()
