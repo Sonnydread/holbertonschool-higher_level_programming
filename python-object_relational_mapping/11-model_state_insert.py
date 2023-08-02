7d58eb497bf39545b8f72d470b9f8e5ba541f6b6
Sonnydread <alvaradoguille590@gmail.com>
2023-08-02 15:02:17 -0700

    model11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """adds the State object “Louisiana” to the hbtn_0e_6_usa"""
 
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from model_state import Base, State
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Sesion = sessionmaker(bind=engine)
     sesion = Sesion()
