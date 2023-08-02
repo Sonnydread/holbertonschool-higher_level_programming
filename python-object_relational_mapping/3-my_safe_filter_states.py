da4ea9f6b6c69cbaaef316c17067a6b367bee827
Sonnydread <alvaradoguille590@gmail.com>
2023-08-02 06:36:13 -0700

    3filtr

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" takes in arguments and displays all values in the states of hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    kursor = db.cursor()
+    kursor.execute(sys.argv[4])
+    rows = kursor.fetchall()
+    for row in rows:
+        print(row)
+    kursor.close()
+    db.close()
