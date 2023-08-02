0dc6a3b19affd05c21cea38e5fdef0b5c16c7f0a
Sonnydread <alvaradoguille590@gmail.com>
2023-08-02 06:39:03 -0700

    3filtr

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     kursor = db.cursor()
-    kursor.execute(sys.argv[4])
+    kursor.execute (sys.argv[4])
     rows = kursor.fetchall()
     for row in rows:
         print(row)
