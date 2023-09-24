d32d75a1475e8aafd4632c736cc143f7237ccbd5
Sonnydread <alvaradoguille590@gmail.com>
2023-09-23 19:52:17 -0500

    script that searchs the character name from this URL

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.get("https://swapi-api.hbtn.io/api/people/5/?format=json",
     function (inf) {
-          let nombre = inf.name;
+          let name = inf.name;
           $("#character").text(nombre);
         });
 });
