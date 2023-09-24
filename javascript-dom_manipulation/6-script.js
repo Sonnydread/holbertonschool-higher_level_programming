a7d18cf10c0dfd27a187e9c1db7d00ee23b431b2
Sonnydread <alvaradoguille590@gmail.com>
2023-09-24 07:33:08 -0500

    search the character name from a  URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+const $character = document.querySelector('#character');
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    $character.textContent = data.name;
+  });
